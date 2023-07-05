8d9f0a919f8f32df884b3a501bab5de21c4e2d72
Ruilin Geng <1585363499@qq.com>
2023-07-05 10:03:06 +0800

    引入配置文件

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # GinProject
 
-Simple Gin Project
+Gin Example Project
 
-运行事项
+## 运行事项
 
-+ 修改common/database.go中的数据库配置
++ 修改config/application.yml中的配置
 
+## 项目特点
+
++ 使用gin作为基本框架
++ 使用gorm框架与MySQL数据库进行交互
++ 引入jwt配合中间件作为用户认证
++ 对Response请求进行封装
--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"GinProject/common"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
-
+	InitConfig()
 	common.InitDB()
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port)) // listen and serve on
+	}
 	err := r.Run()
 	if err != nil {
 		panic(err.Error()) // listen and serve on
 	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
